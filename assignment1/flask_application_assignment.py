27bb35b37395f9cce5d43185a3a2e09f4df1acf4
rrrowannn <rowangrasmeijer8@gmail.com>
2025-11-05 16:31:25 +0100

    updated index file and flask file with 2.b instructions

--- assignment1/flask_application_assignment.py
+++ assignment1/flask_application_assignment.py
@@ -17,22 +17,29 @@ db = SQLAlchemy(app)
 # Movie model representing the movies table
 class Movie(db.Model):
     __table_args__ = {'extend_existing': True}
-     ...
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer)
+    oscars = db.Column(db.Integer)
 
 # Create the database and the tables for the model
 with app.app_context():
     db.create_all()
 
-@app.route('/', methods=[...])
+@app.route('/', methods=['GET'])
 def index():
     movies = Movie.query.all()  # Get all movies from the database
     return render_template('index.html', movies=movies)
 
-@app.route('/add_movie', methods=[...])
+@app.route('/add_movie', methods=['GET', 'POST'])
 def add_movie():
-    if request.method == ... :
-        #get items from form 
-
+    if request.method == 'POST' :
+        #get items from form
+        id = request.form['id']
+        name = request.form['name']
+        year = request.form['year']
+        oscars = request.form['oscars']
+        
         # Create a new movie entry
         new_movie = Movie(...)
 
